6bb560c4c638806cd461635365b5eb8536d5017f
John Ryan <uijedi@gmail.com>
2019-06-02 19:49:45 -0400

    Add POJOs

---
+++ src/main/java/com/sigiliius/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.sigiliius.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/sigiliius/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.sigiliius.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
