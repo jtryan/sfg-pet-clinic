a42c4dfd9fe3dec6442eb8b3d4e0af6f4a35f1b8
John Ryan <uijedi@gmail.com>
2019-07-09 16:11:33 -0400

    Add Service interface for Vet, Pet, and Owner models.

---
+++ pet-clinic-data/src/main/java/com/sigilius/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.sigilius.sfgpetclinic.services;
+
+import com.sigilius.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
